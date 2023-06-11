ded5f0518f152a0e3c9ddaa622a74f868693288c
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-06-11 11:51:40 -0300

    3

--- src/data2.js
+++ src/data2.js
@@ -1,7 +1,9 @@
 const adressPoints=[
-    [51.51, -0.08 , "3"],
-    [55.51, -1.08 , "5"],
+    [51.50, -0.11 , "London Eye"],
+    [51.50, -0.07 , "Tower of London"],
     [51.37, -2.32 , "Bath uni"],
-    [51.50, 0.14 , "Buckingham"],
+    [51.51, -0.12 , "British Museum"],
+    [51.51, -0.09 , "St. Paul's Cathedral"],
+    [51.49,  -0.12 , "Westminster Abbey"],
 ]
 export default adressPoints
